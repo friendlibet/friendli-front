ef425320ff928450140b5ef6b5a28c9b5b9fe365
bengar-dev <benoitgar.pro@gmail.com>
2022-08-31 18:48:27 +0200

    install stack

---
+++ redux/store.ts
@@ -0,0 +1,6 @@
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({});
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
